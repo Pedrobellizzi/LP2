af4daf6c42c41e6c3b529aeff786f36ca1aa0b4a
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-08-23 13:05:06 -0300

    Add files via upload

---
+++ 2.1.1 - ClassesStrucks/Pentagono.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+typedef struct {
+   int x, y;
+   int l1, l2, l3, l4, l5;
+} Pent;
+
+void print (Pent* r) {
+   printf ("Tamanho dos lados do pentagono (%d,%d,%d,%d,%d) / Posicao (%d,%d)\n",
+       r->l1,r->l2,r->l3,r->l4,r->l5, r->x,r->y);
+}
+
+void main (void) {
+   Pent p1 = { 1,1, 15,15,15,15,15 };
+   print(&p1);
+}
+
+
