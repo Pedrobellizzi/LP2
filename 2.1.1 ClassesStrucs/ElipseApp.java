1d18b33b7d10d5bf8fac050398f7008ae4002f72
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-08-23 10:51:56 -0300

    Add files via upload

--- 2.1.1 ClassesStrucs/ElipseApp.java
+++ 2.1.1 ClassesStrucs/ElipseApp.java
@@ -1,1 +1,22 @@
+class Elipse {
+    int x, y;
+    int w, h;
 
+    Elipse (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    void print () {
+        System.out.format("Elipse de tamanho horizontal (%d) e vertical (%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public class ElipseApp {
+         public static void main (String[] args) {
+             Elipse e1 = new Elipse(1,1,20,10);
+             e1.print();
+    }
+}
