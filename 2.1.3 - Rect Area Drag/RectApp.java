14352136b99a25f0aca2c314cc132efaa95e4db7
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-08-25 11:59:53 -0300

    Add files via upload

--- 2.1.3 - Rect Area Drag/RectApp.java
+++ 2.1.3 - Rect Area Drag/RectApp.java
@@ -1,1 +1,48 @@
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class RectApp {
+    public static void main (String[] args) {
+        Rect r1 = new Rect(1,1, 15,25);
+        r1.area();
+	r1.print();
+	r1.drag(5,3);
+	r1.print();
+        r1.drag(12,9);
+	r1.print();
+     }
+} 
+
+class Rect {
+     int x, y;
+     int w, h;
+     int area;
+     int dx, dy;
+     Rect (int x, int y, int w, int h) {
+         this.x = x;
+         this.y = y;
+         this.w = w;
+         this.h = h;
+     }
+
+     int area () {
+	 area = w*h;
+         return area;
+     }
+
+     void drag(int dx, int dy){
+        this.x = this.x + dx;
+        this.y = this.y + dy;
+    }
+
+
+     void print () {  
+       System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d) e com area de (%d).\n", 
+           this.w, this.h, this.x, this.y, this.area);
+       }
+}
+
+
+
