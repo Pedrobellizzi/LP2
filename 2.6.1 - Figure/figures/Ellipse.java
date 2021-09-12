0ac5fe5b13d4ce11a025fc560df3cc1f8fff2b00
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-09-12 19:42:11 -0300

    Add files via upload

--- 2.6.1 - Figure/figures/Ellipse.java
+++ 2.6.1 - Figure/figures/Ellipse.java
@@ -1,14 +1,16 @@
 package figures;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Ellipse2D.Double;
 
 
-public class Rect extends Figure {
+public class Ellipse extends Figure {
     int x, y;
     int w, h;
     int r, g, b, r1, g1, b1;
 
-    public Rect (int x, int y, int w, int h, int r, int g, int b, int r1, int g1, int b1) {
+    public Ellipse (int x, int y, int w, int h, int r, int g, int b, int r1, int g1, int b1) {
         this.x = x;
         this.y = y;
         this.w = w;
@@ -22,16 +24,18 @@ public class Rect extends Figure {
     }
 
     public void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+        System.out.format("Elipse de tamanho (%d,%d) na posicao (%d,%d).\n",
             this.w, this.h, this.x, this.y);
     }
 
-
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g.setColor(new Color(this.r, this.g, this.b));
-        g2d.fillRect(this.x,this.y, this.w,this.h);
+        g.setColor(new Color(this.r, this.g, this.b));        
+        g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
         g.setColor(new Color(this.r1, this.g1, this.b1));
-        g2d.drawRect(this.x,this.y, this.w,this.h);
+        g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
     }
 }
+
+
+
