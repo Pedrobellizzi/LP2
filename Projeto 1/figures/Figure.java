6d9e9551fa8ef6187a5be103b3604c2f33e1c20a
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-09-26 19:59:34 -0300

    Update Figure.java

--- Projeto 1/figures/Figure.java
+++ Projeto 1/figures/Figure.java
@@ -7,8 +7,7 @@ public abstract class Figure {
     public int x, y;
     public int w, h;
     public int r, g, b, r1, g1, b1;
-    public int x2,x3,x4,x5, y2,y3,y4,y5;
-
+    
     public Figure (int x, int y, int w, int h, int r, int g, int b, int r1, int g1, int b1) {
         this.x = x;
         this.y = y;
