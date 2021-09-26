71a8b6d4fd46759029c6a573d9d5ddf8afe091ce
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-09-26 10:05:49 -0300

    Update Line.java

--- 2.6.3 - Figure/figures/Line.java
+++ 2.6.3 - Figure/figures/Line.java
@@ -3,7 +3,8 @@ package figures;
 import java.awt.*;
 
 public class Line extends Figure{
-    
+    public int x2,y2;
+	
     public Line (int x, int y, int x2, int y2, int w, int h, int r, int g, int b, int r1, int g1, int b1) {
         super(x,y, w,h, r,g,b, r1,g1,b1);        
         this.x2 = x2;
@@ -19,6 +20,8 @@ public class Line extends Figure{
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+	g.setColor(new Color(this.r, this.g, this.b));        
+        g2d.fillRect(this.x,this.y, this.w,this.h);
         g.setColor(new Color(this.r1, this.g1, this.b1));
         g2d.drawLine(this.x,this.y, this.x2,this.y2);
     }
