16fe5aa0fc787e5e349b90a9f14e0409d81dd8c1
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-10-10 20:16:43 -0300

    Add files via upload

--- Projeto 1/figures/Pentagono.java
+++ Projeto 1/figures/Pentagono.java
@@ -5,8 +5,8 @@ import java.awt.*;
 public class Pentagono extends Figure {
     public int x1,y1; 
 
-    public Pentagono (int x, int y, int w, int h, int r, int g, int b, int r1, int g1, int b1){
-        super(x,y, w,h, r,g,b, r1,g1,b1);	
+    public Pentagono (int x, int y, int w, int h, Color fundo, Color contorno){
+        super(x,y, w,h, fundo, contorno);	
                 
     }
     
@@ -16,11 +16,11 @@ public class Pentagono extends Figure {
     }
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        int x1[] = {this.x, this.x+30, this.x+60, this.x+45, this.x+15};
-        int y1[] = {this.y, this.y-15, this.y, this.y+35, this.y+35};
-        g.setColor(new Color(this.r, this.g, this.b));
+        int x1[] = {this.x, this.x+(w/2), this.x+w, this.x+((w/4)*3), this.x+(w/4)};
+        int y1[] = {this.y+(h/4), this.y, this.y+(h/4), this.y+h, this.y+h};
+        g.setColor(this.fundo);
 	g2d.fillPolygon(x1, y1, 5);
-	g.setColor(new Color(this.r1, this.g1, this.b1));
+	g.setColor(this.contorno);
         g2d.drawPolygon(x1, y1, 5);
     }
 
