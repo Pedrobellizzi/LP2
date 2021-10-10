936d0d9e294bae50ceb6781d8d6341f11f495873
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-10-10 20:14:56 -0300

    Add files via upload

--- Projeto 1/figures/Ellipse.java
+++ Projeto 1/figures/Ellipse.java
@@ -7,8 +7,8 @@ import java.awt.geom.Ellipse2D.Double;
 
 public class Ellipse extends Figure {
 
-    public Ellipse (int x, int y, int w, int h, int r, int g, int b, int r1, int g1, int b1) {
-        super(x,y, w,h, r,g,b, r1,g1,b1);        
+    public Ellipse (int x, int y, int w, int h, Color fundo, Color contorno) {
+        super(x,y, w,h, fundo, contorno);        
 
     }
 
@@ -19,9 +19,9 @@ public class Ellipse extends Figure {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g.setColor(new Color(this.r, this.g, this.b));        
+        g.setColor(this.fundo);        
         g2d.fill(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
-        g.setColor(new Color(this.r1, this.g1, this.b1));
+        g.setColor(this.contorno);
         g2d.draw(new Ellipse2D.Double(this.x,this.y, this.w,this.h));
     }
 }
