cef649bae3beb59715bb8126967669256452b557
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-10-10 20:17:32 -0300

    Add files via upload

--- Projeto 1/figures/Triangle.java
+++ Projeto 1/figures/Triangle.java
@@ -4,8 +4,8 @@ import java.awt.*;
 
 public class Triangle extends Figure {
     
-    public Triangle(int x, int y, int w, int h, int r, int g, int b, int r1, int g1, int b1){
-        super(x,y, w,h, r,g,b, r1,g1,b1);
+    public Triangle(int x, int y, int w, int h, Color fundo, Color contorno){
+        super(x,y, w,h, fundo, contorno);
      
     }
 
@@ -17,9 +17,9 @@ public class Triangle extends Figure {
         Graphics2D g2d = (Graphics2D) g;
         int x1[] = {this.x, ((this.w) / 2) + (this.x), (this.w) + (this.x)};
         int y1[] = {this.y + h, this.y, this.y + h};
-	g.setColor(new Color(this.r, this.g, this.b)); 
+	g.setColor(this.fundo); 
         g2d.fillPolygon(x1, y1, 3);
-	g.setColor(new Color(this.r1, this.g1, this.b1));
+	g.setColor(this.contorno);
         g2d.drawPolygon(x1, y1, 3);
     }
 
