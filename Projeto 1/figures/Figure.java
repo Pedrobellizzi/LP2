bc1a792a9ab1d7f4a7b68b7c7537db1b82272504
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-10-10 20:15:33 -0300

    Add files via upload

--- Projeto 1/figures/Figure.java
+++ Projeto 1/figures/Figure.java
@@ -7,24 +7,23 @@ public abstract class Figure {
 
     public int x, y;
     public int w, h;
-    public int r, g, b, r1, g1, b1;
+    public Color fundo;
+    public Color contorno;
+   
     
-    public Figure (int x, int y, int w, int h, int r, int g, int b, int r1, int g1, int b1) {
+    public Figure (int x, int y, int w, int h, Color fundo, Color contorno) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.r1 = r1;
-        this.g1 = g1;
-        this.b1 = b1;
+        this.fundo = fundo;
+        this.contorno = contorno;
     }
 
     public boolean clicked (int coordX, int coordY) {
           return (((x <= coordX) && (y <= coordY)) && ((coordX <= (x + w)) && (coordY <= (y + h))));
     }
+
 
     public abstract void paint (Graphics g);
 
