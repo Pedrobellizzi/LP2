8b26df114c460f3ca966f22e05f21b33ddaf0857
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-11-14 00:37:54 -0300

    Delete IStackable.java

--- 4.4.1 - Pilha/Teste/IStackable.java
+++
@@ -1,6 +0,0 @@
-
-public interface IStackable {
-    public int  size ();       // quantos elementos
-    public void push (int v);  // empilha inteiro
-    public int  pop  ();       // desempilha inteiro
-}
