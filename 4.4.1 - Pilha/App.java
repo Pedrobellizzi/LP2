2c23dd4fdd353e5ab0d1fc310db41345deb6f173
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-11-18 09:32:48 -0300

    Update App.java

--- 4.4.1 - Pilha/App.java
+++ 4.4.1 - Pilha/App.java
@@ -8,7 +8,6 @@ class App {
         IStackable s1 = new LinkedStack();
         s1.push(10);
         s1.push(20);
-        s1.push(30);
         int v1  = s1.pop();
         int sz1 = s1.size();
         System.out.format("v=%d, sz=%d\n", v1, sz1);
@@ -16,7 +15,6 @@ class App {
         IStackable s2 = new ArrayStack();
         s2.push(10);
         s2.push(20);
-        s2.push(30);
         int v2  = s2.pop();
         int sz2 = s2.size();
         System.out.format("v=%d, sz=%d\n", v2, sz2);
