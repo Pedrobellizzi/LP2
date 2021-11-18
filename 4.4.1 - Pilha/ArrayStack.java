3432da9aceb831b92e155edc7ff3d89bd46b2b9c
Pedro Antonio Bellizzi <71474814+Pedrobellizzi@users.noreply.github.com>
2021-11-18 09:34:11 -0300

    Update ArrayStack.java

--- 4.4.1 - Pilha/ArrayStack.java
+++ 4.4.1 - Pilha/ArrayStack.java
@@ -6,15 +6,13 @@ public class ArrayStack implements IStackable {
 
        ArrayList<Integer> s2 = new ArrayList<Integer>();
        int sz2 = 0;
-       int v2;
-
-       public void push(int v2){
-             s2.add(v2);
+       
+       public void push(int v){
+             s2.add(v);
              sz2++;
        }
 
        public int pop(){
-//            s2.remove(sz2);
             sz2--; 
             return s2.remove(s2.size()-1);
        }       
